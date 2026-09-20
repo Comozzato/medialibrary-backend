af70d3754a6966cc07289cc4604334ac53ed3799
wille ferreira <dev.willeferreira@gmail.com>
2026-09-20 02:48:15 -0400

    feat: adicionando o spring security e testando a authenticação

---
+++ src/main/java/com/wille/template/controller/AuthController.java
@@ -0,0 +1,38 @@
+package com.wille.template.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wille.template.security.JwtTokenService;
+
+@RestController 
+public class AuthController {
+    @Autowired
+    private JwtTokenService jwtTokenService;
+
+    @PostMapping("/auth/login")
+   public String login(@RequestBody LoginRequest loginRequest) {
+      // Lógica para validar o usuário (no exemplo, apenas um nome de usuário simples)
+      if ("user".equals(loginRequest.getUsername()) && "password".equals(loginRequest.getPassword())) {
+          return jwtTokenService.generateToken(loginRequest.getUsername());
+      }
+      throw new RuntimeException("Credenciais inválidas");
+    }
+
+
+    public class LoginRequest {
+        private String username;
+        private String password;
+
+        public String getUsername() {
+            return username;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+    }
+}
+
---
+++ src/main/java/com/wille/template/security/JwtTokenService.java
@@ -0,0 +1,44 @@
+package com.wille.template.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Service
+public class JwtTokenService {
+
+    private static final String SECRET_KEY =
+            "minha-chave-secreta-com-pelo-menos-32-caracteres";
+
+    private final SecretKey key = Keys.hmacShaKeyFor(
+            SECRET_KEY.getBytes(StandardCharsets.UTF_8)
+    );
+
+    
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(
+                        new Date(System.currentTimeMillis() + 600000)
+                )
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String getUsernameFromToken(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getSubject();
+    }
+}
